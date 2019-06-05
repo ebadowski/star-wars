f541e975c31ec557579abfc55a115f41267b4b30
Zeke <ezekielbadowski@gmail.com>
2019-06-04 21:15:21 -0500

    added table.html

--- server.js
+++ server.js
@@ -46,6 +46,8 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
+
+
 app.get("/add", function(req, res) {
   res.sendFile(path.join(__dirname, "add.html"));
 });
